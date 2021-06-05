e0de746805f9b39f878b4eed201ed845b92532fb
Satria Aji Putra <satriamaxt@gmail.com>
2021-06-05 10:00:51 +0700

    Initial commit

---
+++ app/src/main/java/com/satria/utspraktik_if2_10118068_satriaajiputra/MainActivity.java
@@ -0,0 +1,20 @@
+package com.satria.utspraktik_if2_10118068_satriaajiputra;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/*
+    Tanggal Pengerjaan: 05 Juni 2021
+    NIM: 10118068
+    Nama: Satria Aji Putra Karma J
+    Kelas: IF-2
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
